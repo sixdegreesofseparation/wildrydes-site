260949851552058454ef303f5e55317b4674a5ac
panuchart <47457213+sixdegreesofseparation@users.noreply.github.com>
2020-08-14 22:21:39 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_13wM5avfS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '61mbjpbj9o92e8nhnh0tidi3oh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
