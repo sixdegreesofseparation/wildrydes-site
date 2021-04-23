f95dc25211976e4785b45180ef84acb1a85259ef
panuchart <47457213+sixdegreesofseparation@users.noreply.github.com>
2021-04-23 15:06:33 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_13wM5avfS', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '61mbjpbj9o92e8nhnh0tidi3oh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_I4Cj45gR8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7asrkhsubt3e5k9lmnlbckkpm4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://5m9d0vvaxb.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' //'https://5m9d0vvaxb.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
